e0944029d9b019ac3d40228a08568afdfa9f6ae9
Pis4ni <pis4wrk@gmail.com>
2023-09-03 15:54:58 +0200

    add: milestone 3

--- js/app.js
+++ js/app.js
@@ -3,15 +3,47 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      
+      newMessage:'',
       contacts:contacts,
       activeChat:0,
-
+      actualTime:formattedDate,
+      atuoReply: false
     }
   },
   methods:{
     goTo(index){
       this.activeChat = index
     },
-  }
+    addNewMessage(activeChat){
+      const newMessageObj={
+        date: this.actualTime,
+        message: this.newMessage,
+        status: 'sent'
+      }
+      this.contacts[this.activeChat].messages.push(newMessageObj)
+      this.newMessage =''
+      this.setAutoReply()
+      console.log('funge')
+
+    },
+    handleKeyDown(event) {
+      if (event.key == 'Enter' || event.code == 'Enter') this.addNewMessage(this.activeChat) 
+    },
+    autoMessage(activeChat){
+      const newMessageObj={
+        date: this.actualTime,
+        message: 'ok',
+        status: 'received'
+      }
+      this.contacts[this.activeChat].messages.push(newMessageObj)
+    },
+    setAutoReply(){
+      this.atuoReply = setTimeout(()=>{
+        this.autoMessage(this.activeChat)
+      },2000)
+    }
+  },
+  created() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  },
 }).mount('#app')
--- js/data.js
+++ js/data.js
@@ -170,3 +170,19 @@ const contacts= [
     }
     ]
 console.table(contacts);
+
+
+const now = new Date();
+
+const day = String(now.getDate()).padStart(2, '0');
+const month = String(now.getMonth() + 1).padStart(2, '0'); // Nota: i mesi in JavaScript partono da 0, quindi aggiungiamo 1
+const year = now.getFullYear();
+
+const hours = String(now.getHours()).padStart(2, '0');
+const minutes = String(now.getMinutes()).padStart(2, '0');
+const seconds = String(now.getSeconds()).padStart(2, '0');
+
+const formattedDate = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+
+console.log(formattedDate);
+
